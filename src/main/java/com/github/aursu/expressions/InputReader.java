b66b27201c4c7a5e58d5aa50500c5c1f8107c1cb
Alexander Ursu <alexander.ursu@gmail.com>
2022-10-14 01:20:21 +0200

    Replaced InputReader with own to expose current position

--- src/main/java/com/github/aursu/expressions/InputReader.java
+++ src/main/java/com/github/aursu/expressions/InputReader.java
@@ -1,44 +1,55 @@
 package com.github.aursu.expressions;
 
-import java.io.IOException;
-import java.io.StringReader;
-
-public class InputReader extends StringReader {
+/*
+ * Similar to StringReader
+ * support lookahead peek() methods
+ * current position inside string is available
+ * 
+ * see https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/io/StringReader.html
+ */
+public class InputReader {
+	private String buf;
+	private int count;
+	private int pos = 0;
 
 	public InputReader(String buffer) {
-		super(buffer);
+		setInput(buffer);
 	}
 
-	// peek next character
-	public char peek(long n) throws IOException {
-		// save current position 
-		mark(0);
-		
-		// skip n characters till the end of the stream
-		skip(n);
-		
-		// read next character after skip
-		int c = read();
-		
-		// reset position to the save
-		reset();
-		
-		if (c == -1) return 0;
-		return (char) c;
+	public void setInput(String buffer) {
+		buf = buffer;
+		count = buffer.length();
+		pos = 0;
+	}
+
+	public char peek(int n) {
+        int index = pos + Math.min(count - pos, n < 0 ? 0 : n);
+
+        if (index < count)
+            return buf.charAt(index);
+
+        return 0;
     }
 
-	public char peek() throws IOException {
+	public char peek() {
         return peek(0);
     }
 
-	public char next() throws IOException {
-    	int c = read();
+    public int getPos() {
+		return pos;
+	}
 
-		if (c == -1) return 0;
-		return (char) c;
+	public char next() {
+    	char c = peek();
+
+    	// avoid to increase current position unnecessary 
+    	if (c != 0) pos++;
+
+        return c;
     }
 
-    public boolean eof() throws IOException {
+    public boolean eof() {
         return peek() == 0;
     }
 }
+
