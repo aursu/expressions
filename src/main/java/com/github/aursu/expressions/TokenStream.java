18e67327636ba0821c897890dcaf902f35ade0a3
Alexander Ursu <alexander.ursu@gmail.com>
2022-10-14 14:33:30 +0200

    Moved all number parsing routines into NumberToken class

--- src/main/java/com/github/aursu/expressions/TokenStream.java
+++ src/main/java/com/github/aursu/expressions/TokenStream.java
@@ -3,9 +3,6 @@ package com.github.aursu.expressions;
 public class TokenStream {
 	// input stream
 	private InputReader input;
-
-	// current input character
-	private char peek;
 	
 	// token start position inside input stream
 	private int start = 0;
@@ -15,7 +12,6 @@ public class TokenStream {
 
 	public TokenStream(String buffer) {
 		input = new InputReader(buffer);
-		peek = input.peek();
 	}
 	
 	public Token<?> peek() {
@@ -31,17 +27,22 @@ public class TokenStream {
     protected Token<?> read() {
     	// if end of input 
     	if (input.eof()) return Token.EOFToken;
+     	
+    	// lookahead
+    	char peek = input.peek();
     	
-    	// read character
+    	// save start position
     	setStart(input.getPos());
-    	peek = input.next();
     	
     	// skip whitespace (for arithmetic operations is meaningless)
-    	if (isWhitespace(peek)) return read();
-    	
+    	if (isWhitespace(peek)) {
+    		input.next();  // consume whitespace
+    		return read();
+    	}
+
     	// check if number token
-    	if (NumberToken.isNumber(peek, input.peek(), current)) {
-    		String number = readNumber();
+    	if (NumberToken.isNumber(input, current)) {
+    		String number = NumberToken.readNumber(input);
     		
     		if (number == null) return null;
     		
@@ -54,6 +55,8 @@ public class TokenStream {
 
     	// check if operator
     	if (OperatorToken.isOperator(peek)) {
+    		input.next();  // consume operator
+
     		OperatorToken token = OperatorToken.getToken(peek);
     		
     		token.setRange(start, input.getPos());
@@ -62,6 +65,8 @@ public class TokenStream {
 
     	// check if separator
     	if (SeparatorToken.isSeparator(peek)) {
+    		input.next();  // consume separator
+  
     		SeparatorToken token = SeparatorToken.getToken(peek);
    
     		token.setRange(start, input.getPos());
@@ -74,82 +79,6 @@ public class TokenStream {
 	public boolean isWhitespace(char ch) {
     	return ch == ' ' || ch == '\t' || ch == '\n';
     }
-
-	// number  := integer | integer '.' fraction
-	// fraction := digit | fraction digit
-	// integer := '0' | natural | '-' natural
-	// natural := nonzero | natural digit
-	// digit   := '0' | nonzero
-	// nonzero  := '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-	private String readNumber() {
-		String token = readInteger();
-
-		if (token == null) return null;
-		
-		// check float number
-		if (input.peek() == '.') {
-			input.next(); // skip dot
-
-			peek = input.next();
-			
-			String fraction = readFraction();
-			
-			if (fraction == null) return null;
-			
-			return token + "." + fraction;
-		}
-
-		return token;
-	}
-
-	// fraction := digit | fraction digit
-	private String readFraction() {
-		// fraction is a set of digits
-		if (!NumberToken.isDigit(peek)) return null;
-		
-		String token = "" + peek;
-		
-		while (NumberToken.isDigit(input.peek())) {
-			peek = input.next();
-			token += peek;
-		}
-
-		return token;
-	}
-
-	// integer := '0' | natural | '-' natural
-	private String readInteger() {
-		String token = "" + peek;
-		if (peek == '0') return token;
-		if (peek == '-') {
-			peek = input.next(); // skip minus sign
-			
-			String natural = readNatural();
-
-			// something is wrong
-			if (natural == null) return null;
-			
-			return token + natural;
-		}
-		return readNatural();
-	}
-
-	// natural := nonzero | natural digit
-	// digit   := '0' | nonzero
-	// nonzero  := '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-	private String readNatural() {
-		// natural number can not start from zero
-		if (!NumberToken.isNonZero(peek)) return null;
-		
-		String token = String.valueOf(peek);
-
-		while (NumberToken.isDigit(input.peek())) {
-			peek = input.next();
-			token += peek;
-		}
-
-		return token;
-	}
 
 	public int getStart() {
 		return start;
