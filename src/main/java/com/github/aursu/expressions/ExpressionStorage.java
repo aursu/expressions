03412b91b8cc756e2982f135dc55a846c1ec917c
Alexander Ursu <alexander.ursu@gmail.com>
2022-10-20 16:33:50 +0200

    Added check() method to check if key exists in db

--- src/main/java/com/github/aursu/expressions/ExpressionStorage.java
+++ src/main/java/com/github/aursu/expressions/ExpressionStorage.java
@@ -13,14 +13,37 @@ public class ExpressionStorage extends DatabaseAccess {
 		super(dbName, dbHost, dbUser, dbPassowrd);
 	}
 
-	public Vector<Vector<Object>> load() {
+	public Vector<Vector<Object>> load() throws SQLException {
 		return load("SELECT * FROM expressions;");
 	}
 
-	public Vector<Vector<Object>> lookup(double lkpValue, OperatorToken op) {
+	private Vector<Vector<Object>> check(String prn) {
+		String sql = "SELECT * FROM expressions WHERE Polish = ?";
+
+		try {
+			PreparedStatement pstmt = prepare(sql);
+			pstmt.setString(1, prn);
+
+			ResultSet rs = pstmt.executeQuery();
+			Vector<Vector<Object>> rowData = readResult(rs);
+
+			pstmt.close();
+
+			return rowData;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public Vector<Vector<Object>> lookup(double lkpValue, OperatorToken op) throws SQLException {
 		String sql;
 
-		switch(op.getValue()) {
+		if (op == null)
+			sql = "SELECT * FROM expressions WHERE ABS(Value - ?) < ?";
+		else
+			switch(op.getValue()) {
 			case CMP_GT:
 				sql = "SELECT * FROM expressions WHERE Value > ?";
 				break;
@@ -33,90 +56,85 @@ public class ExpressionStorage extends DatabaseAccess {
 			case CMP_LTE:
 				sql = "SELECT * FROM expressions WHERE Value < ? OR ABS(Value - ?) < ?";
 				break;
-			// https://dev.mysql.com/doc/refman/8.0/en/problems-with-float.html
+				// https://dev.mysql.com/doc/refman/8.0/en/problems-with-float.html
 			case EQ_NE:
 				sql = "SELECT * FROM expressions WHERE ABS(Value - ?) > ?";
 				break;
 			case EQ_EQ:
 			default:
 				sql = "SELECT * FROM expressions WHERE ABS(Value - ?) < ?";
-		}
-		
+			}
+
 		PreparedStatement pstmt;
 		ParameterMetaData pstmtmeta;
 
-		if (this.isConnected()) {
-			try {
-				pstmt = prepare(sql);
-				pstmtmeta = pstmt.getParameterMetaData();
+		if (isConnected()) {
+			pstmt = prepare(sql);
+			pstmtmeta = pstmt.getParameterMetaData();
 
-				pstmt.setDouble(1, lkpValue);
-				// parameters count
-				switch(pstmtmeta.getParameterCount()) {
-					case 2:
-						pstmt.setDouble(2, ExpressionStorage.tolerance);
-						break;
-					case 3:
-						pstmt.setDouble(2, lkpValue);
-						pstmt.setDouble(3, ExpressionStorage.tolerance);
-				}
-
-				ResultSet rs = pstmt.executeQuery();
-				Vector<Vector<Object>> rowData = readResult(rs);
-				pstmt.close();
-				
-				return rowData;
-			} catch (SQLException e) {
-				e.printStackTrace();
+			pstmt.setDouble(1, lkpValue);
+			// parameters count
+			switch(pstmtmeta.getParameterCount()) {
+			case 2:
+				pstmt.setDouble(2, ExpressionStorage.tolerance);
+				break;
+			case 3:
+				pstmt.setDouble(2, lkpValue);
+				pstmt.setDouble(3, ExpressionStorage.tolerance);
 			}
+
+			ResultSet rs = pstmt.executeQuery();
+			Vector<Vector<Object>> rowData = readResult(rs);
+			pstmt.close();
+
+			return rowData;
+
 		}
 		return null;
 	}
 
-	public int delete(String prn) {
+	public int delete(String prn) throws SQLException {
 		String sql = "DELETE FROM expressions WHERE Polish = ?";
 
-		try {
-			PreparedStatement pstmt = prepare(sql);
-			pstmt.setString(1, prn);
-			
-			int rows = pstmt.executeUpdate();
-			pstmt.close();
-			
-			return rows;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return 0;
+		PreparedStatement pstmt = prepare(sql);
+		pstmt.setString(1, prn);
+
+		int rows = pstmt.executeUpdate();
+		pstmt.close();
+
+		return rows;
 	}
-	
+
 	public int store(String prn, String infix, Number evalValue) throws SQLException {		
 		return store(prn, infix, evalValue.doubleValue());
 	}
 
-	public int store(String prn, String infix, double evalValue) throws SQLException {
-		String sql = "INSERT INTO expressions VALUES (?, ?, ?)";
+	public int store(String prn, String infix, double evalValue) throws SQLException {		
+		Vector<Vector<Object>> peek = check(prn);
+
+		// replace Database row with same primary key
+		String sql = "REPLACE INTO expressions VALUES (?, ?, ?)";
+
+		// if not exists - insert it
+		if (peek == null || peek.isEmpty())
+			sql = "INSERT INTO expressions VALUES (?, ?, ?)";
 
 		PreparedStatement pstmt = prepare(sql);
 		pstmt.setString(1, prn);
 		pstmt.setString(2, infix);
 		pstmt.setDouble(3, evalValue);
-		
+
 		int rows = pstmt.executeUpdate();
 		pstmt.close();
-		
+
 		return rows;
 	}
 
-	private Vector<Vector<Object>> load(String sql) {
+	private Vector<Vector<Object>> load(String sql) throws SQLException {
 		if (this.isConnected()) {
-			try {
-				ResultSet rs = query(sql);
+			ResultSet rs = query(sql);
 
-				return readResult(rs);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			return readResult(rs);
 		}
 		return null;
 	}
@@ -126,15 +144,15 @@ public class ExpressionStorage extends DatabaseAccess {
 
 		while(rs.next()) {
 			Vector<Object> row = new Vector<>();
-            int numColumns = rs.getMetaData().getColumnCount();
-            for (int i = 1; i <= numColumns; i++) {
-                // Column numbers start at 1.
-                // Also there are many methods on the result set to return
-                // the column as a particular type. Refer to the Sun documentation
-                // for the list of valid conversions.
-            	row.add(rs.getObject(i));
-            }
-            rowData.add(row);
+			int numColumns = rs.getMetaData().getColumnCount();
+			for (int i = 1; i <= numColumns; i++) {
+				// Column numbers start at 1.
+				// Also there are many methods on the result set to return
+				// the column as a particular type. Refer to the Sun documentation
+				// for the list of valid conversions.
+				row.add(rs.getObject(i));
+			}
+			rowData.add(row);
 		}
 		rs.close();
 
